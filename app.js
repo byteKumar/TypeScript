87d66bf241525203f9567696c025c7de77f0c356
Chaman Kumar <chamankumar5181@gmail.com>
2024-11-29 04:57:29 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,115 @@
+"use strict";
+var a = 12;
+let arr = [1, 2, 3, 4, 5];
+let arr1 = ['hello', 10];
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["USER"] = "user";
+    UserRoles["SUPER_ROLE"] = "super_role";
+})(UserRoles || (UserRoles = {}));
+console.log(UserRoles.ADMIN);
+let b;
+b = 12;
+b = 'hello';
+if (typeof b === 'string') {
+    b.toUpperCase();
+}
+function abcd(user) {
+    console.log(user.name);
+}
+function abcde(user) {
+    console.log(user.admin);
+}
+abcde({ name: 'hello', age: 12, gender: 'male', admin: true });
+abcd({ name: 'hello', age: 12, gender: 'male' });
+let c = 12;
+function abcdef(user) {
+    console.log(user.age);
+    user.getDetails('hello');
+}
+class Device {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class phone extends Device {
+    constructor(name) {
+        super(name);
+    }
+    getDetails() {
+        console.log(this.name);
+    }
+}
+let di = new phone("milton");
+// let device = new Device();
+// let device1 = new Device();
+class HumanMaker {
+    constructor(name, age, age1) {
+        this.age = age;
+        this.age1 = age1;
+        this.name = name;
+    }
+}
+let human = new HumanMaker('hello', 12);
+class chaman {
+    constructor() {
+        this.name = 'chaman';
+    }
+    getDetails() {
+        this.name;
+    }
+}
+class BottleMaker {
+    constructor() {
+        this.name = 'chamann';
+    }
+}
+class BottleMaker1 extends BottleMaker {
+    constructor() {
+        super(...arguments);
+        this.materal = 'metal';
+    }
+}
+let bottle = new BottleMaker1();
+class user2 {
+    constructor(name) {
+        this.name = name;
+    }
+}
+let user1 = new user2('chaman');
+class chaman1 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getNames() {
+        return this.name;
+    }
+    get name1() {
+        return this.name;
+    }
+    set name2(name) {
+        this.name = name;
+    }
+    setNames(name) {
+        this.name = name;
+    }
+    getAge() {
+        return this.age;
+    }
+    setAge(age) {
+        this.age = age;
+    }
+}
+let chaman2 = new chaman1('chaman', 12);
+chaman2.getNames();
+chaman2.setNames('hello');
+chaman2.getAge();
+chaman2.setAge(13);
+chaman2.name1;
+chaman2.name2 = 'hello';
+class snell {
+}
+snell.namekumar = 'chaman';
+snell.name;
---
+++ app.ts
@@ -0,0 +1,163 @@
+var a = 12;
+
+let arr: number[] = [1, 2, 3, 4, 5];
+
+let arr1: [string, number] = ['hello', 10];
+
+enum UserRoles {
+    ADMIN = "admin",
+    USER =  "user",
+    SUPER_ROLE = "super_role"
+}
+
+console.log(UserRoles.ADMIN);
+
+let b:unknown;
+
+b=12;
+b='hello';
+
+if (typeof b === 'string') {
+    b.toUpperCase();
+}
+
+interface User {
+    name: string,
+    age: number,
+    gender?: string
+}
+
+interface ADMIN extends User {
+    admin: boolean;
+}
+
+function abcd(user: User) {
+    console.log(user.name);
+}
+
+function abcde(user: ADMIN) {
+    console.log(user.admin);
+}
+
+abcde({name: 'hello', age: 12, gender: 'male', admin: true});
+
+abcd({name: 'hello', age: 12, gender: 'male'});
+
+
+type sankhaya = number;
+let c: sankhaya = 12;
+
+type user = {
+    name: string,
+    age: number
+}
+
+type admin = user & {
+    getDetails(user: string): void;
+}
+
+function abcdef(user: admin) {
+    console.log(user.age);
+    user.getDetails('hello');
+}
+
+class Device {
+    constructor(public name: string) {}
+}
+
+class phone extends Device {
+    constructor(name: string) {
+        super(name);
+    }
+
+    getDetails() {
+        console.log(this.name);
+    }
+}
+
+let di = new phone("milton");
+
+// let device = new Device();
+// let device1 = new Device();
+
+class HumanMaker {
+    public name: string;
+    constructor(name: string, public age: number, public age1?: number){
+        this.name = name;
+    }
+}
+
+let human = new HumanMaker('hello', 12);
+
+
+class chaman {
+    name = 'chaman';
+
+    getDetails() {
+        this.name;
+    }
+}
+
+
+class BottleMaker {
+    public name = 'chamann';
+}
+
+class BottleMaker1 extends BottleMaker {
+    public materal = 'metal';
+}
+
+let bottle = new BottleMaker1();
+
+class user2 {
+    constructor(public readonly name: string) {}
+
+    // changeName() {
+    //     this.name = 'hello';
+    // }
+}
+
+let user1 = new user2('chaman');
+
+class chaman1 {
+    constructor(public name: string, public age: number) {}
+
+    getNames() {
+        return this.name;
+    }
+
+    get name1() {
+        return this.name;
+    }
+
+    set name2(name: string) {
+        this.name = name;
+    }
+
+    setNames(name: string) {
+        this.name = name;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    setAge(age: number) {
+        this.age = age;
+    }
+}
+
+let chaman2 = new chaman1('chaman', 12);
+chaman2.getNames();
+chaman2.setNames('hello');
+chaman2.getAge();
+chaman2.setAge(13);
+chaman2.name1;
+chaman2.name2 = 'hello';
+
+
+class snell {
+    static namekumar = 'chaman';
+}
+
+snell.name;
